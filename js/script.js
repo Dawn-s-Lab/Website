c10380cd9b290e40a07002524f4b6203c6fa57c7
Dawn <DevilishDawn43@proton.me>
2026-02-12 19:27:23 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+// Select the element
+const button = document.getElementById('button');
+// Define the function to run when the button is clicked
+function handleClick() {
+  alert("Hello World! :3")
+}
 
+// Add the event listener
+button.addEventListener('click', handleClick);
